063fb0912c4d95ef28f6b57d762e0cb01e7926c4
Allan Rigoberto Rosales Matamoros <159342763+LanRosMata@users.noreply.github.com>
2025-06-15 22:33:15 -0600

    Se creo parte de la estructura principal del sitio

---
+++ src/utilitys/imagensElements.js
@@ -0,0 +1,155 @@
+
+import lgUPHimg from '../assets/img/image2.svg'
+
+//imgenes Fintech mundo
+import alipayIMG from '../assets/img/f-mundo/image4.png'
+import paylapIMG from '../assets/img/f-mundo/image5.png'
+import blockIMG from '../assets/img/f-mundo/image6.png'
+import strapiIMG from '../assets/img/f-mundo/image7.png'
+
+//Fintech Latam
+import nubankIMG from '../assets/img/f-latam/image8.png'
+import pixIMG from '../assets/img/f-latam/image9.png'
+
+// finctech centro américa
+import chivoIMG from '../assets/img/f-ca/image10.png'
+import bitcoinIMG from '../assets/img/f-ca/image11.png'
+
+// finctech Honduras INDEL
+import tigoIMG from '../assets/img/f-hn-indel/image12.png'
+import tengoIMG from '../assets/img/f-hn-indel/image13.png'
+import diloIMG from '../assets/img/f-hn-indel/image14.png'
+import kashIMG from '../assets/img/f-hn-indel/image15.png'
+
+// finctech Honduras EPSPE
+import pixelPayIMG from '../assets/img/f-hn-epspe/image16.png'
+import pagaditoIMG from '../assets/img/f-hn-epspe/image17.png'
+import ginihIMG from '../assets/img/f-hn-epspe/image18.jpeg'
+import subeIMG from '../assets/img/f-hn-epspe/image19.png'
+
+// Imagenes de stock
+import foto1IMG from '../assets/img/stock/image3.jpeg'
+import foto2IMG from '../assets/img/stock/image20.jpeg'
+import foto3IMG from '../assets/img/stock/image21.jpeg'
+import foto4IMG from '../assets/img/stock/image22.jpeg'
+import foto5IMG from '../assets/img/stock/image23.jpeg'
+import foto6IMG from '../assets/img/stock/image24.jpeg'
+import foto7IMG from '../assets/img/stock/image29.jpeg'
+import foto8IMG from '../assets/img/stock/image30.jpeg'
+import foto9IMG from '../assets/img/stock/image31.jpeg'
+import foto10IMG from '../assets/img/stock/image32.jpeg'
+
+
+
+const imagensElements = {
+    lgUPH: {
+      img: lgUPHimg,
+      alt: 'Logo Universidad Politécnica de Honduras'
+    },
+    alipay:{
+      img : alipayIMG,
+      alt: 'Alipay'
+    },
+    paylpal:{
+      img : paylapIMG,
+      alt: 'Paylpal'
+    },
+    block:{
+      img : blockIMG,
+      alt: 'Block'
+    },
+    strapi:{
+      img : strapiIMG,
+      alt: 'Strapi'
+    },
+    nubank:{
+      img : nubankIMG,
+      alt: 'Nubank'
+    },
+    pix:{
+      img : pixIMG,
+      alt: 'Pix'
+    },
+    chivo:{
+      img : chivoIMG,
+      alt: 'Chivo Wallet'
+    },
+    bitcoin:{
+      img : bitcoinIMG,
+      alt: 'Bitcoin'
+    },
+    tigo:{
+      img : tigoIMG,
+      alt: 'Tigo Money'
+    },
+    tengo:{
+      img : tengoIMG,
+      alt: 'Tengo'
+    },
+    dilo:{
+      img : diloIMG,
+      alt: 'Dilo'
+    },
+    kash:{
+      img : kashIMG,
+      alt: 'Kash'
+    },
+    pixelPay:{
+      img : pixelPayIMG,
+      alt: 'Pixel Pay'
+    },
+    pagadito:{
+      img : pagaditoIMG,
+      alt: 'Pagadito'
+    },
+    ginih:{
+      img : ginihIMG,
+      alt: 'Ginih'
+    },
+    sube:{
+      img : subeIMG,
+      alt: 'Sube'
+    },
+    foto1:{
+      img : foto1IMG,
+      alt: 'Foto 1'
+    },
+    foto2:{
+      img : foto2IMG,
+      alt: 'Foto 2'
+    },
+    foto3:{
+      img : foto3IMG,
+      alt: 'Foto 3'
+    },
+    foto4:{
+      img : foto4IMG,
+      alt: 'Foto 4'
+    },
+    foto5:{
+      img : foto5IMG,
+      alt: 'Foto 5'
+    },
+    foto6:{
+      img : foto6IMG,
+      alt: 'Foto 6'
+    },
+    foto7:{
+      img : foto7IMG,
+      alt: 'Foto 7'
+    },
+    foto8:{
+      img : foto8IMG,
+      alt: 'Foto 8'
+    },
+    foto9:{
+      img : foto9IMG,
+      alt: 'Foto 9'
+    },
+    foto10:{
+      img : foto10IMG,
+      alt: 'Foto 10'
+    },
+};
+
+export default imagensElements
